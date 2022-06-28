9bea0322a8a155b0d3118081daf5bde9930eebac
manaswinieadara <96780506+manaswinieadara@users.noreply.github.com>
2022-06-28 22:14:44 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,(n//2)+1):         if n%i==0:             return 0     return n x=int(input()) y=int(input()) i=1 c=0 v=x+y while i!=0:     n=v+i     if prime(n):         m=n         break     i+=1 print(abs(v-m))

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,12 +1,19 @@
 def prime(n):
-    while n:
-        c=0
-        for i in range(2,n//2):
-            if n%i==0:
-                c+=1
-        if c==0:
-            return n
-        n+=1
-a=int(input())
-b=int(input())
-print(prime(a+b+1)-(a+b))
+    if n==1:
+        return 0
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return 0
+    return n
+x=int(input())
+y=int(input())
+i=1
+c=0
+v=x+y
+while i!=0:
+    n=v+i
+    if prime(n):
+        m=n
+        break
+    i+=1
+print(abs(v-m))    
